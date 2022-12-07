5567dbb5bb1984ec8b92e2684eddb4b758647033
Prince Muel <vansomecsam@gmail.com>
2022-12-07 22:45:59 +0100

    feat: setup react-router, add styles in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
+import '@src/assets/styles/main.css';
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
 
-render(
+const root = createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path="/*" element={<App />} />
       </Routes>
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
