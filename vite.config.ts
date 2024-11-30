ed6940642f08344c3f8015d55c3cd00ff2855e13
Prince Muel <vansomecsam@gmail.com>
2024-11-30 04:40:36 +0100

    feat(styles): migrate tailwindcss to v4 beta

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { reactRouter as router } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   envPrefix: "PUBLIC_",
+  server: { port: 3000 },
   plugins: [
+    // @ts-expect-error
+    tailwindcss(),
     router(),
     svgr({
       svgrOptions: {
