a4d48f4e0b24d5589189f0db54cbec8d0bf3dca9
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-05-29 13:00:46 +0100

    chore: cleanup unnecessary files

--- app/assets/index.ts
+++ app/assets/index.ts
@@ -1,3 +1,1 @@
-export { default as pattern } from "./images/pattern.png";
-export { default as placeholder } from "./images/placeholder.avif";
-export { default as avatar } from "./images/princemuel-open-graph-image-2024.jpg";
+export * from "@/assets/images";
