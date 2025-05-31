1ca67017c245ddf3253dcce39810c0ecb271ab65
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-05-31 02:41:05 +0100

    feat: update metadata tags generation

--- app/constants/settings.ts
+++ app/constants/settings.ts
@@ -1,3 +1,5 @@
+// eslint-disable no-object-as-default-parameter
+// eslint-disable func-style
 export const getSiteSettings = () => ({
   id: "settings",
   /** The site title*/
@@ -26,6 +28,49 @@ export const getSiteSettings = () => ({
   published_date: new Date("2024-02-01T16:43:29.577Z"),
 });
 
+type PageTitleContent = string | [pageTitle: string, isStandAlone?: boolean];
+type TitleTemplate = string;
+interface TemplateContext {
+  [key: string]: string;
+}
+
+/**
+ * Formats the HTML title based on the provided content and template.
+ * If content is an array, it expects the first element to be the page title
+ * and the second to indicate if it's a standalone page.
+ * If content is a string, it uses that as the page title.
+ * The template can include placeholders for title and brand.
+ *
+ * @param content - The page title content, either a string or an array.
+ * @param template - The title template with placeholders.
+ * @param context - Additional context for placeholders in the template.
+ * @returns The formatted HTML title string.
+ */
+export function formatHtmlTitle(
+  content?: PageTitleContent,
+  template: TitleTemplate = "%title% | %brand%",
+  context: TemplateContext = { brand: "Prince Muel" },
+): string {
+  const fallbackTitle = `${context.brand} | Developer, Educator & Musician`;
+
+  if (!content) return fallbackTitle;
+
+  if (Array.isArray(content)) {
+    const [pageTitle, isStandAlone] = content;
+    if (isStandAlone) return pageTitle || fallbackTitle;
+
+    return template.replaceAll(/%(\w+)%/gu, (match, key) => {
+      if ("title" === key) return pageTitle;
+      return context[key] || match;
+    });
+  }
+
+  return template.replaceAll(/%(\w+)%/gu, (match, key) => {
+    if ("title" === key) return content;
+    return context[key] || match;
+  });
+}
+
 export const baseData = {
   title: "Cesar Poumian's Blog",
   description: "Cesar Poumian's blog. Web development, programming, and personal projects.",
@@ -36,3 +81,91 @@ export const baseData = {
     "Below are all my recent blog posts. Some of them are tutorials, others are project walk-throughs/case-studies, and some are insights discussing things like libraries, courses, developer resources, etc.",
   blog_title: "My Posts",
 };
+
+export const delimiter = "–";
+export const published_date = new Date("2024-02-01T16:43:29.577Z");
+
+export const homeKeywords = [
+  "princemuel",
+  "iamprincemuel",
+  "princemuel_cs",
+  "pHoeniX-svg",
+  "Prince Muel",
+  "Samuel Chukwuzube",
+  "Software Engineer",
+  "Frontend Engineer",
+  "Web Developer",
+  "React Developer",
+  "Coding Instructor",
+  "Musician",
+  "Educator",
+  "React",
+  "Node.js",
+  "GraphQL",
+  "JavaScript",
+  "Typescript",
+  "Frontend Development",
+  "Web Design",
+  "UI/UX",
+  "Web Applications",
+  "Tech Industry",
+  "Portfolio",
+  "Africa",
+];
+
+export const defaultKeywords = [
+  "React",
+  "Qwik",
+  "Astro",
+  "Remix",
+  "Next.js",
+  "JavaScript",
+  "TypeScript",
+  "Styled Components",
+  "TailwindCSS",
+  "Jest",
+  "Vitest",
+  "Rust",
+  "Testing",
+  "React Testing Libary",
+  "Node.js",
+  "MongoDB",
+  "Jamstack",
+  "Component Library",
+  "Serverless Functions",
+  "Edge Functions",
+  "SQL",
+  "GraphQL",
+  "Postgres",
+  "Linux",
+  "Security",
+  "Homelab",
+  "Bash",
+  "Interview",
+  "Engineering",
+  "Competition",
+  "Cloudflare",
+  "Database",
+  "Devops",
+  "Oss",
+  "Git",
+  "Gatsby",
+  "Golang",
+  "Projects",
+  "Performance",
+  "Personal",
+  "System Design",
+  "Networking",
+  "Frontend Development",
+  "Web Design",
+  "UI/UX",
+];
+
+export interface NewsletterFormInput {
+  email: string;
+  first_name?: string;
+  last_name?: string;
+  from_url?: string;
+}
+
+export type PostClickedFrom = "recent" | "suggested" | "previous" | "next";
