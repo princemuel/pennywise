4dc528d6287b89654f252583bd670c8d2984edf6
Prince Muel <68931805+princemuel@users.noreply.github.com>
2026-01-19 15:41:59 +0100

    ci: create script to generate icons as React components

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { reactRouter } from '@react-router/dev/vite';
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
-import devtoolsJson from 'vite-plugin-devtools-json';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import svgr from 'vite-plugin-svgr';
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import devtoolsJson from "vite-plugin-devtools-json";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	envPrefix: 'PUBLIC_',
-	plugins: [devtoolsJson(), tailwindcss(), svgr(), reactRouter(), tsconfigPaths()],
-	server: { host: true },
-	define: { __BUILD_DATE__: JSON.stringify(new Date()) }
+  envPrefix: "PUBLIC_",
+  plugins: [devtoolsJson(), tailwindcss(), svgr(), reactRouter(), tsconfigPaths()],
+  server: { host: true, port: 3000 },
+  define: { __BUILD_DATE__: JSON.stringify(new Date()) },
 });
