6106413848bf1e65618c6b036b2229d3de556410
Prince Muel <68931805+princemuel@users.noreply.github.com>
2026-01-30 01:59:52 +0100

    ci: configure eslint and prettier

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+export default {
+  typedRoutes: true,
+  reactCompiler: true,
+  experimental: {
+    authInterrupts: true,
+    viewTransition: true,
+    webVitalsAttribution: ["CLS", "LCP", "INP"],
+    // Enable filesystem caching for `next dev`
+    turbopackFileSystemCacheForDev: true,
+    // Enable filesystem caching for `next build`
+    turbopackFileSystemCacheForBuild: true,
+  },
+} satisfies NextConfig;
