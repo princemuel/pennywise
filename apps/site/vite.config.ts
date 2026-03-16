3c24835680028d980accf1235c35b5583d8cc7e8
Prince Muel <68931805+princemuel@users.noreply.github.com>
2026-03-16 14:55:30 +0100

    chore: remove uncessary pkgs and config
    Signed-off-by: Prince Muel <68931805+princemuel@users.noreply.github.com>

--- apps/site/vite.config.ts
+++ apps/site/vite.config.ts
@@ -2,9 +2,8 @@ import svg from "@poppanator/sveltekit-svg";
 import { enhancedImages } from "@sveltejs/enhanced-img";
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { playwright } from "@vitest/browser-playwright";
+import { defineConfig } from "vite";
 import devtoolsJson from "vite-plugin-devtools-json";
-import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   envPrefix: "PUBLIC_",
@@ -19,33 +18,5 @@ export default defineConfig({
     sveltekit(),
     devtoolsJson()
   ],
-  define: { __BUILD_DATE__: JSON.stringify(new Date()) },
-  test: {
-    expect: { requireAssertions: true },
-    projects: [
-      {
-        extends: "./vite.config.ts",
-        test: {
-          name: "client",
-          browser: {
-            enabled: true,
-            provider: playwright(),
-            instances: [{ browser: "chromium", headless: true }]
-          },
-          include: ["src/**/*.svelte.{test,spec}.{js,ts}"],
-          exclude: ["src/lib/server/**"]
-        }
-      },
-
-      {
-        extends: "./vite.config.ts",
-        test: {
-          name: "server",
-          environment: "node",
-          include: ["src/**/*.{test,spec}.{js,ts}"],
-          exclude: ["src/**/*.svelte.{test,spec}.{js,ts}"]
-        }
-      }
-    ]
-  }
+  define: { __BUILD_DATE__: JSON.stringify(new Date()) }
 });
